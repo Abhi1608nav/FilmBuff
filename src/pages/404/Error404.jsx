97576a9d49123f44fb9de4b9ec368d34edc558b8
Abhi1608nav <abhinavshakya1608@gmail.com>
2024-06-08 13:36:45 +0530

    Updated the ErrorPage

--- src/pages/404/Error404.jsx
+++ src/pages/404/Error404.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
+
 import "./style.scss";
 
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+
 const Error404 = () => {
-  return (
-    <div>Error404</div>
-  )
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
 }
 
 export default Error404
